4af0ad5182bffcaaacf170a2b766bb68ad3fdd05
prateek <whyprateek@gmail.com>
2017-11-25 16:17:19 -0800

    chrome fire url only once

--- popup/background.js
+++ popup/background.js
@@ -1,22 +1,47 @@
-var BackgroundColor="RED";
+var BackgroundColor="";
+//
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// chrome.tabs.executeScript(null,
+//       {code:"document.body.style.backgroundColor='" + BackgroundColor + "'"});
+//
+//
+// chrome.tabs.executeScript({
+//     file: 'alert.js'
+//   });
+// });
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//   alert(document.window.href);
+// }
+chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
+    var url = tab.url;
+        if (url !== undefined && changeinfo.status == "complete") {
+          alert(tab.url);
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='" + BackgroundColor + "'"});
-     
-
-chrome.tabs.executeScript({
-    file: 'alert.js'
-  }); 
+    }
 });
+// chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+//     // if(details.frameId === 0) {
+//     //     // Fires only when details.url === currentTab.url
+//     //     chrome.tabs.get(details.tabId, function(tab) {
+//     //         if(tab.url === details.url) {
+//     //           alert(tab.url);
+//     //         }
+//     //     });
+//     // }
+// });
+// chrome.notifications.create('teast', {
+//     type: 'basic',
+//     iconUrl: 'icon.png',
+//     title: 'Don\'t forget!',
+//     message: 'You have things to do. Wake up, dude!'
+//  }, function(notificationId) {});
 
-chrome.tabs.onCreated.addListener(function(tab) {         
-   
-chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='" + BackgroundColor + "'"});
-
-chrome.tabs.executeScript({
-    file: 'alert.js'
-  }); 
-});
+// chrome.tabs.onCreated.addListener(function(tab) {
+//
+// chrome.tabs.executeScript(null,
+//       {code:"document.body.style.backgroundColor='" + BackgroundColor + "'"});
+//
+// chrome.tabs.executeScript({
+//     file: 'alert.js'
+//   });
+// });
