6c69faed9b67892447bfd5f776b8f3e1aa804a3b
prateek <whyprateek@gmail.com>
2017-11-25 21:12:20 -0800

    cleaning code

--- popup/background.js
+++ popup/background.js
@@ -1,17 +1,3 @@
-var BackgroundColor="";
-//
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-// chrome.tabs.executeScript(null,
-//       {code:"document.body.style.backgroundColor='" + BackgroundColor + "'"});
-//
-//
-// chrome.tabs.executeScript({
-//     file: 'alert.js'
-//   });
-// });
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//   alert(document.window.href);
-// }
 chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
     var url = tab.url;
         if (url !== undefined && changeinfo.status == "complete") {
@@ -22,7 +8,7 @@ chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
           // Notify that we saved.
           alert('save');
             chrome.storage.local.get('PhisingId', function(result){
-              alert(result.PhisingId);  
+              alert(result.PhisingId);
               });
         });
             chrome.notifications.create(url, {
@@ -34,29 +20,3 @@ chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
           }
     }
 });
-// chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-//     // if(details.frameId === 0) {
-//     //     // Fires only when details.url === currentTab.url
-//     //     chrome.tabs.get(details.tabId, function(tab) {
-//     //         if(tab.url === details.url) {
-//     //           alert(tab.url);
-//     //         }
-//     //     });
-//     // }
-// });
-// chrome.notifications.create('teast', {
-//     type: 'basic',
-//     iconUrl: 'icon.png',
-//     title: 'Don\'t forget!',
-//     message: 'You have things to do. Wake up, dude!'
-//  }, function(notificationId) {});
-
-// chrome.tabs.onCreated.addListener(function(tab) {
-//
-// chrome.tabs.executeScript(null,
-//       {code:"document.body.style.backgroundColor='" + BackgroundColor + "'"});
-//
-// chrome.tabs.executeScript({
-//     file: 'alert.js'
-//   });
-// });
