0c37fe4d8aaf0664d6986ed45df589f86ee614e3
prateek <whyprateek@gmail.com>
2017-11-27 14:54:31 -0800

    demo check api

--- phishing-backend/routes/index.js
+++ phishing-backend/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/check', function(req, res, next) {
+  return res.status(200).json({'status': 200, 'message' : 'success'});
+})
+
 module.exports = router;
