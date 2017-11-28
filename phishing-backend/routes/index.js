b15e336ae93b6092cc5867f5e7f17a1df5eb8317
Sai Supraja Malla <saisupraja.malla@sjsu.edu>
2017-11-28 01:05:17 -0800

    Addings mysql

--- phishing-backend/routes/index.js
+++ phishing-backend/routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'phishing'
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
